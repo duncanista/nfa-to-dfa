3756aceec9388a14b0ba5b17e162964b843b6910
duncanista <30836115+duncanista@users.noreply.github.com>
2019-06-27 21:09:00 -0500

    Add files
    Sample input added, also formated .py and created UI for the algorithm

--- NFAtoDFA.py
+++ NFAtoDFA.py
@@ -1,82 +1,180 @@
+# coding: utf-8
 import re
+from tkinter import *
+from tkinter import filedialog
 from collections import defaultdict
 from itertools import chain, combinations
 
-##Power set function taken from itertools recipes
+## Power set function taken from itertools recipes
 def powerset(iterable):
-    "list(powerset([1,2,3])) --> [(), (1,), (2,), (3,), (1,2), (1,3), (2,3), (1,2,3)]"
+    "list(powerset([1,2,3])) --> [(), (1), (2), (3), (1,2), (1,3), (2,3), (1,2,3)]"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
-nfaInput = "{(0,p,p),(0,p,q),(1,p,p),(0,q,r),(1,q,r),(0,r,s),(0,s,s),(1,s,s)}"
+## Retrieve NFA given its transitions
+def getTransitions(nfa):
+    transitions = []
+    for transition in nfa:
+        transitions.append(transition.split(","))
+    return transitions
 
-nfaInput = re.findall('\(([^)]+)',nfaInput)
+## Get the NFA states
+def getNFAStates(transitions):
+    Q = [element for sublist in transitions for element in sublist]
+    Q = list(set(Q))
+    if '0' in Q:
+        Q.remove('0')
+    if '1' in Q:
+        Q.remove('1')
+    if '_' in Q:
+        Q.remove('_')
+    Q.sort()
 
-transitionList = []
-for transition in nfaInput:
-    transitionList.append(transition.split(","))
+    return Q
 
-##Creando la lista de Q para el NFA
-nfaQ = [element for sublist in transitionList for element in sublist]
-nfaQ = list(set(nfaQ))
-if '0' in nfaQ:
-    nfaQ.remove('0')
-if '1' in nfaQ:
-    nfaQ.remove('1')
-if '_' in nfaQ:
-    nfaQ.remove('_')
-nfaQ.sort()
+## Get the DFA final states given the NFA states
+def getDFAStates(nfaQ):
+    Q = []
+    for q in powerset(nfaQ):
+        Q.append(list(q))
 
-dfaQ = []
-for q in powerset(nfaQ):
-    dfaQ.append(list(q))
+    return Q
 
-
-nfaDelta = defaultdict(list)
-entry = ''
-for entry,origin,target in transitionList:
-    if entry == '0':
+## NFA delta given its transitions
+def getNFADelta(transitions):
+    delta = defaultdict(list)
+    for entry, origin, target in transitions:
         index = 0
-    elif entry == '1':
-        index = 1
-    else:
-        entry = 2
+        if entry == '0':
+            index = 0
+        elif entry == '1':
+            index = 1
 
-    if origin not in nfaDelta:
-        nfaDelta[origin] = [[],[]]
+        if origin not in delta:
+            delta[origin] = [[], []]
 
-    nfaDelta[origin][index].append(target)
+        delta[origin][index].append(target)
 
-print(nfaDelta)
+    return delta
 
-resultList = []
+## DFA delta given its states, and the NFA delta
+def getDFADelta(Q, nfaDelta):
+    delta = []
 
-for q in dfaQ: #por cada subset de Q
-    result = [q, [],[]]
-    for state in q: #para cada Q del NFA en el subset de Q del DFA
-        for i in nfaDelta[state][0]:
-            result[1].append(i)
-        for i in nfaDelta[state][1]:
-            result[2].append(i)
-        result[1] = list(set(result[1]))
-        result[2] = list(set(result[2]))
-        result[1].sort()
-        result[2].sort()
-    resultList.append(result)
+    # for every Q subset
+    for q in Q:
+        result = [q, [], []]
+        # for every NFA Q in the subset of Q in the DFA
+        for state in q:
+            for i in nfaDelta[state][0]:
+                result[1].append(i)
+            for i in nfaDelta[state][1]:
+                result[2].append(i)
+            result[1] = list(set(result[1]))
+            result[2] = list(set(result[2]))
+            result[1].sort()
+            result[2].sort()
+        delta.append(result)
 
-formattedResult = [["Q", "0", "1"]]
-for line in resultList:
-    newLine = []
-    for element in line:
-        if element == []:
-            new = "{/}"
-        else:
-            new = "{" + ", ".join(element) + "}"
-        newLine.append(new)
-    formattedResult.append(newLine)
+    return delta
+
+## Set NFA final state as the last element thats being pointed in the raw input
+def getNFAFinal(nfa):
+    last = nfa[-1]
+    last = last.split(",")[-1]
+    return last
+
+## Calculate the DFA final states by checking if the NFA final is in both zero and one from delta.
+def getDFAFinal(final, delta):
+    finalStates = []
+    for state, zero, one  in delta:
+        if final in zero and final in one:
+            finalStates.append(state)
+    return finalStates
+
+def formatStates(Q):
+    cardinality = len(Q[0])
+    f = "\t"
+    for q in Q:
+        if len(q) > cardinality:
+            f += "\n\t"
+            cardinality = len(q)
+        f += "{" + ", ".join(q) + "}, "
+    f = f[:-2]
+    return f
+
+def formatDelta(delta):
+    result = [["Q", "0", "1"]]
+    for line in delta:
+        newLine = []
+        for element in line:
+            if element == []:
+                new = "{/}"
+            else:
+                new = "{" + ", ".join(element) + "}"
+            newLine.append(new)
+        result.append(newLine)
+
+    return result
+
+def getNFA(file):
+    return re.findall('\(([^)]+)', file)
+
+def getFile():
+    path = filedialog.askopenfilename(filetypes=(("Text files", "*.txt"), ("all files", "*.*")))
+    file = open(path, 'r').read()
+    return file
+
+def convert():
+    global app
+    file = getFile()
+    nfaInput = getNFA(file)
+    getNFAFinal(nfaInput)
+
+    transitions = getTransitions(nfaInput)
+
+    nfaQ = getNFAStates(transitions)
+    nfaDelta = getNFADelta(transitions)
+    nfaFinal = getNFAFinal(nfaInput)
+
+    dfaQ = getDFAStates(nfaQ)
+    dfaQo = nfaQ[0]
+    dfaDelta = getDFADelta(dfaQ, nfaDelta)
+    dfaFinal = getDFAFinal(nfaFinal, dfaDelta)
+    dfaFormatDelta = formatDelta(dfaDelta)
+
+    new = open('result.txt', 'w+')
+    new.write("=" * 60)
+    new.write("\nDefinición formal del DFA convertido.\n\nN = {Q, E, d, Qo, F} \n\n")
+    new.write("Q = { \n" + formatStates(dfaQ) + "\n}\n\n")
+    new.write("E = {0, 1}\n\n")
+    new.write("Qo = " + dfaQo + "\n\n")
+    new.write("F = { \n" + formatStates(dfaFinal) + "\n}\n\n")
+    new.write("d = { \n")
+
+    frmt = '{:20}' + 2 * '{:20}'
+    for line in dfaFormatDelta:
+        new.write("\t" + (frmt.format(*line)) + "\n")
+
+    new.write("}")
+
+    result = Label(app, text="Se creó un archivo con la definición formal.", font = ("Arial", 14))
+    result.grid(pady=5, row=2, column=0)
+
+def main():
+    global app
+    app = Tk()
+    app.title("NFA to DFA application.")
+    label = Label(app, text="Busque el archivo donde tiene su NFA y desea convertir a DFA.", font=("Arial", 14))
+    button = Button(app, text="Seleccionar archivo...", command=convert)
+
+    label.grid(pady=5, row=0, column=0)
+    button.grid(pady=5, row=1, column=0)
+    app.mainloop()
+
+global app
+main()
 
 
 
-frmt = '{:20}' + 2 * '{:20}'
-for line in formattedResult:
-    print(frmt.format(*line))
+
